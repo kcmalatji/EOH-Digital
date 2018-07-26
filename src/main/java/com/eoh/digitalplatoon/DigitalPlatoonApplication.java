c1b2fad67699c367af7ba950115f35c2451d9015
Khomotso Chris Malatji <ABKMAA5@eng.absa.co.za>
2018-07-26 09:45:22 +0200

    first commit

---
+++ src/main/java/com/eoh/digitalplatoon/DigitalPlatoonApplication.java
@@ -0,0 +1,12 @@
+package com.eoh.digitalplatoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalPlatoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalPlatoonApplication.class, args);
+	}
+}
